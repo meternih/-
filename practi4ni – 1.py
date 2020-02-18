e53d98aef2d250b97f83bbce4771726bc859422d
IVAN IVANSKYI <60467969+meternih@users.noreply.github.com>
2020-02-18 16:10:28 +0200

    practi4ni

---
+++ practi4ni – 1.py
@@ -0,0 +1,34 @@
+#порівняти числа
+# a=int(input("Enter first number "))
+# b=int(input("Enter second number "))
+# if a > b:
+#     print("ma4o")
+# else:
+#     print("neMa40")
+
+
+#парне/непарне
+#a=int(input(" Enter  number ")) 
+#if a % 2 == 0:
+ #   if a % 2 == 1:
+  #      print("yes")
+   # else:
+    #    print("no")
+
+
+#n = int(input()) 
+#factorial = 1
+#while n > 1:
+#    factorial *= n
+#    n -= 1
+#print(factorial)
+
+
+start = 0
+finish = 10
+while start < finish:
+    print(start)
+    start += 1
+else:
+    print (“The end”)
+
