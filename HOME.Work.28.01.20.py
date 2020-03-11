2f7d7b5e43072ac889da9315f9398a762442fa6f
IVAN IVANSKYI <60467969+meternih@users.noreply.github.com>
2020-03-11 13:40:01 +0200

    Add files via upload

---
+++ HOME.Work.28.01.20.py
@@ -0,0 +1,14 @@
+a = int(input("Put your numer a : "))
+b = int(input("Put your numer b : "))
+
+print ("a + b = ", a + b)
+print ("a - b = ", a - b)
+print ("a / b = ", a / b)
+print ("a * b = ", a * b)
+
+################################################
+
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+adress = input("Where do you live?: ")
+print ("Hello " + name + " You are " + str(age) + " You live in " + adress)
